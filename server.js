18b6087436486adf99af88edcfd42b4eb650e5a9
Chris Ball <chris@echobind.com>
2014-09-23 22:48:10 -0400

    Add redis pub/sub channel. Use redis-backed socket.io adapter.

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var redis = require('redis');
+var redisClient = redis.createClient();
 
+// use socket.io-redis if you need lots of connections. it allows
+// socket.io to work across servers
+var socketIORedis = require('socket.io-redis');
+io.adapter(socketIORedis({ host: 'localhost', port: 6379 }));
+
+redisClient.subscribe('new-app-version');
+
+redisClient.on('message', function(channel, message) {
+  if(channel == 'new-app-version') {
+    io.emit('new app version', message);
+  }
+});
+
+// server config
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-
-  socket.on('new app version', function(msg){
-    io.emit('new app version', msg);
-  });
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
-
 http.listen(3000, function(){
   console.log('listening on *:3000');
+  console.log('publish something to the new-app-version Redis channel (redis-cli publish new-app-version whoa!)');
+  console.log('---');
 });
